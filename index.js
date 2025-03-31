1758f2b83306a6c24455c87ef029261069930093
Ansh Gupta <anshgupta651@gmail.com>
2025-03-31 17:08:49 +0530

    input from Command line

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 
 
-const http =require('http');
-const data= require('./data');
-http.createServer((req,resp)=>{
-    resp.writeHead(200,{'content-type':'application\json'});
-    resp.write(JSON.stringify(data));
-    resp.end();
+// const http =require('http');
+// const data= require('./data');
+// http.createServer((req,resp)=>{
+//     resp.writeHead(200,{'content-type':'application\json'});
+//     resp.write(JSON.stringify(data));
+//     resp.end();
 
-}).listen(5000);
+// }).listen(5000);
+
+//console.log(process.argv[3]);
+const fs= require('fs');
+const input = process.argv;
+if(input[2]=='add')
+fs.writeFileSync(input[3],input[4]);
+else if(input[2]=='remove')
+    fs.unlinkSync(input[3]);
+else 
+console.log('invalid input');
