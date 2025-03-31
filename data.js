5e2cb706168928bdf1a482b7772e3e59bae1321b
Ansh Gupta <anshgupta651@gmail.com>
2025-03-31 13:18:22 +0530

    API

---
+++ data.js
@@ -0,0 +1,2 @@
+const data=[{name: 'Ansh Gupta',email:'ag@gmail.com', Sex:'M'},{name: 'Anshika Gupta',email:'ag@gmail.com',Sex:'F'},{name: 'Arpit Gupta',email:'ag@gmail.com',Sex:'M'}]
+module.exports=data;
--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 
-console.log("hello");
-console.log(10+20);
-// const chalk = require('chalk');
 
-// console.log(chalk.blue('Hello world!'));
-// const colors= require('colors');
-// console.log("fuck".bgBlue);
-// import chalk from 'chalk';
+const http =require('http');
+const data= require('./data');
+http.createServer((req,resp)=>{
+    resp.writeHead(200,{'content-type':'application\json'});
+    resp.write(JSON.stringify(data));
+    resp.end();
 
-// console.log(chalk.green('Hello, world!'));
-// const fs= require('fs').writeFileSync;
-
-// // fs.writeFileSync("hello.txt","mkc bhai");
-
-// fs("king.txt","kohli is gr8");
-
-// console.log(__dirname);
-
-// const http = require('http');
-
-// http.createServer((req,resp)=>{
-//     resp.write("<h1>hello this is ansh gupta</h1>");
-//     resp.end();
-// }).listen(4500);
+}).listen(5000);
