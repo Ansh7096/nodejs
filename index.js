743cc4c0ae94dde3b3ed95a71043d24a03c45f05
Ansh Gupta <anshgupta651@gmail.com>
2025-03-31 19:04:11 +0530

    Promises

--- index.js
+++ index.js
@@ -50,10 +50,15 @@
 let a=10;
 let b=0;
 
-setTimeout(()=>{
-    b=20;
-},2000);
-console.log(a+b);
+let waitingData= new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve(30);
+    },2000);
+})
+waitingData.then((data)=>{
+    b=data;
+    console.log(a+b);
+})
 
 //This is the drawback of async programming language
 
