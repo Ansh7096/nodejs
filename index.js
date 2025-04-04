ef2a86420331b83d65fad38ecf9525dfb23a0a1f
Ansh Gupta <anshgupta651@gmail.com>
2025-04-04 19:17:52 +0530

    Connecting Pages

--- index.js
+++ index.js
@@ -116,6 +116,20 @@ const path= require('path');
 const app= express();
 const publicPath= path.join(__dirname,'public');
 
-app.use(express.static(publicPath));
+//app.use(express.static(publicPath));
+app.get('',(_,resp)=>{
+    resp.sendFile(`${publicPath}/index.html`);
+})
+app.get('/about',(_,resp)=>{
+    resp.sendFile(`${publicPath}/about.html`);
+})
+app.get('/home',(_,resp)=>{
+    resp.sendFile(`${publicPath}/home.html`);
+})
+app.get('/*splat', (_, resp) => {
+    resp.sendFile(path.join(publicPath, 'pnf.html'));
+});
+
+
 
 app.listen(5000);
